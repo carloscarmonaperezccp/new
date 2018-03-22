53e35ec18e44fe3f46fe369776098222ae02545b
carloscarmonaperezccp <35309781+carloscarmonaperezccp@users.noreply.github.com>
2018-03-22 00:16:27 -0600

    Add files via upload

---
+++ WEB_PAGE/bienvenidoo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Bienvenido al sistema</title>
+<link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+
+<h1> Bienvenido al sistema </h1>
+
+
+
+<script src="js/jquery-3.1.1.min.js"></script>    
+<script src="js/main.js"></script>
+</body>
+</html>
